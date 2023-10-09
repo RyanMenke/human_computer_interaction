2005429b2c6bc122c7c4f6905097392d91b2be2e
RyanMenke <58480868+RyanMenke@users.noreply.github.com>
2023-10-09 14:52:50 -0400

    first commit

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,52 @@
+import * as React from "react"
+
+import { Button } from "../components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "../components/ui/card"
+import { Input } from "../components/ui/input"
+import { Label } from "../components/ui/label"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "../components/ui/select"
+
+export default function CardWithForm() {
+    return (
+        <main className="flex h-screen w-screen flex-col items-center bg-zinc-900">
+            <div className="flex w-full h-12 bg-gradient-to-r from-purple-600 to-indigo-500"/>
+            <h1 className="text-6xl font-bold p-[5%] text-gray-100">Learn Link</h1>
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>Sign up</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="name">Email</Label>
+                            <Input id="name" placeholder="Enter email address" />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="framework">Password</Label>
+                            <Input type="password" id="name" placeholder="Create password" />
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Button variant="outline">Back</Button>
+                <Button>Submit</Button>
+            </CardFooter>
+        </Card>
+        </main>
+    )
+}
---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import Learnlink from './Untitled_Artwork 5.png'
+import {Button} from "../components/ui/button";
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <main className="flex h-screen w-screen flex-col items-center bg-zinc-900">
+            <div className="flex w-full h-12 bg-gradient-to-r from-purple-600 to-indigo-500"/>
+            <h1 className="text-6xl font-bold p-[5%] text-gray-100">Learn Link</h1>
+            <Image src={Learnlink} className="h-[30%] w-[30%]" alt="Logo"/>
+            <Button className="px-24 py-6 m-6" asChild>
+                <Link href="/sign-up">Sign up</Link>
+            </Button>
+            <Button variant="outline" className="px-24 py-6 m-6" asChild>
+                <Link href="/sign-in">Sign in</Link>
+            </Button>
+        </main>
+    )
+}
+ 
